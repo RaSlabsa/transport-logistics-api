c241317fe1ab5683cb0ec1bea342a902ea2803ca
Maksum <95537230+WebBlade@users.noreply.github.com>
2025-12-07 03:40:19 +0200

    Added AutoMapping and MappingProfile

---
+++ TransportLogistics.Services/Mappings/MappingProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using TransportLogistics.Core.Entities;
+using TransportLogistics.Core.DTOs.Get;
+using TransportLogistics.Core.DTOs.Post;
+
+namespace TransportLogistics.Services.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Client, ClientDto>();
+            CreateMap<ClientCreateDto, Client>();
+
+            CreateMap<Driver, DriverDto>();
+            CreateMap<DriverCreateDto, Driver>();
+
+            CreateMap<Order, OrderDto>();
+            CreateMap<OrderCreateDto, Order>();
+
+            CreateMap<Trip, TripDto>();
+            CreateMap<TripCreateDto, Trip>();
+
+            CreateMap<TripLog, TripLogDto>();
+            CreateMap<TripLogCreateDto, TripLog>();
+
+            CreateMap<Vehicle, VehicleDto>();
+            CreateMap<VehicleCreateDto, Vehicle>();
+        }
+    }
+}
