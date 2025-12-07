c0de0eaa77a723a9bcd4824c4126fa4b1c45de17
Maksum <95537230+WebBlade@users.noreply.github.com>
2025-12-07 02:51:03 +0200

    added implementations of CRUD operations

---
+++ TransportLogistics.Repositories/Implementation/GenericRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TransportLogistics.Repositories.Interfaces;
+using TransportLogistics.Repositories.Data;
+
+namespace TransportLogistics.Repositories.Implementation
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly TransportLogicDB _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public GenericRepository(TransportLogicDB context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+        public void Update (T entity)
+        {
+            _dbSet.Attach(entity);
+            _dbSet.Entry(entity).State = EntityState.Modified;
+        }
+        public void Delete (T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
